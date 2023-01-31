c2741a210d2d6fb7ea5d1f91117859c3b58566af
eddipietro <94018665+eddipietro@users.noreply.github.com>
2023-01-31 18:02:05 -0300

    Desafio clase-28-utilizar process

--- src/controller/getObjectRandom.js
+++ src/controller/getObjectRandom.js
@@ -6,25 +6,21 @@ const getObject = () => {
     let arr = [];
     let data = {};
   
-    //Crea un array de 0 -1000, utiliza del 1 al 1000
     for (let i = 0; i <= 1000; i++) {
       arr[i] = 0;
     }
   
-    //Utiliza las posiciones del array para guardar el conteo del numero random
     for (let i = 0; i <= cant; i++) {
       let numRandom = Math.floor(Math.random() * (1001 - 1) + 1);
       arr[numRandom]++;
     }
   
-    //Crae el objeto de salida clave - valor
     for (let i = 0; i <= arr.length - 1; i++) {
       data[i] = {
         vecesQueAparece: arr[i],
       };
     }
-  
-    //console.table(data)
+
     return data;
   };
   
